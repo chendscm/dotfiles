38f0fc5578c1f03e86ce6d855cfd98ec64cdd9be
chendscm <chend.scm@gmail.com>
2025-12-29 20:56:23 +0800

    Add guix G1a

---
+++ guix/G1a/config.scm
@@ -0,0 +1,160 @@
+;; This is an operating system configuration generated
+;; by the graphical installer.
+;;
+;; Once installation is complete, you can learn and modify
+;; this file to tweak the system configuration, and pass it
+;; to the 'guix system reconfigure' command to effect your
+;; changes.
+
+
+;; Indicate which modules to import to access the variables
+;; used in this configuration.
+(use-modules (gnu)
+	     (gnu services docker)
+	     (gnu services pm)
+	     (gnu services syncthing)
+             (nongnu packages linux)
+             (nongnu packages compression)
+             (nongnu system linux-initrd))
+(use-service-modules cups desktop networking ssh xorg)
+
+(operating-system
+  (kernel linux)
+  (initrd microcode-initrd)
+  (firmware (list linux-firmware sof-firmware))
+
+  (locale "en_HK.utf8")
+  (timezone "Asia/Shanghai")
+  (keyboard-layout (keyboard-layout "us"))
+  (host-name "G1a")
+
+  ;; The list of user accounts ('root' is implicit).
+  (users (cons* (user-account
+                  (name "chend")
+                  (comment "Chend")
+                  (group "users")
+                  (home-directory "/home/chend")
+                  (supplementary-groups '("wheel" "netdev" "audio" "video" "kvm")))
+                %base-user-accounts))
+
+  ;; Packages installed system-wide.  Users can also install packages
+  ;; under their own account: use 'guix search KEYWORD' to search
+  ;; for packages and 'guix install PACKAGE' to install a package.
+  (packages
+   (append
+    (map specification->package
+	 '(
+	   ;; system
+	   "amd-microcode"
+	   "xf86-video-intel" "xf86-video-amdgpu"
+	   "mesa" "mesa-utils"
+	   "xorg-server-xwayland" "wayland-utils" "qtwayland"
+	   ;"glibc"
+	   "make"
+	   "gtk" "gcc-toolchain" "dbus"
+	   "curl" "tar" "zenity" ;; lazycat
+	   "tlp"
+	   "xrandr"
+	   "bluez" "brightnessctl" "playerctl"
+	   ;; alsa
+	   "alsa-utils"
+	   ;; fonts
+	   "fontconfig" "font-gnu-unifont" "font-wqy-zenhei" "font-wqy-microhei"
+	   ;; fcitx
+	   "fcitx5" "fcitx5-gtk" "fcitx5-qt" "fcitx5-configtool"
+	   "fcitx5-rime" "librime"
+	   ;"dconf"
+	   ;; tools
+	   "git" "docker"
+	   "zip" "unzip"
+	   ;; edit
+	   "emacs"
+	   ;; sway
+	   ;"sway" "swaylock" "swayidle" "swaybg" "waybar"
+	   ;"wmenu" "polkit" "dconf-editor" "dmenu"
+	   ;; terminal
+	   ;"st" "alacritty"
+	   ;; develop
+	   ;"python" "python-ipython"
+	   ;"python-scipy" "python-requests"
+	   ;; virtual
+	   ;"qemu"
+	   ;; browser
+	   ;"firefox"
+	   ;; vnc
+	   ;"tigervnc-client" "freerdp" "xrdp" "remmina"
+	   ;"moonlight-qt"
+	   ;; game
+	   ;"steam"
+	   ;"font-liberation"
+	   ))
+    %base-packages))
+  
+  ;; Below is the list of system services.  To search for available
+  ;; services, run 'guix system search KEYWORD' in a terminal.
+  (services
+   (cons*
+    (service openssh-service-type)
+    (service cups-service-type)
+    (service bluetooth-service-type
+	     (bluetooth-configuration
+	      (auto-enable? #t)))
+    (service docker-service-type)
+    (service containerd-service-type)
+;    (service gnome-desktop-service-type)
+    (service tlp-service-type
+	     (tlp-configuration
+	      (cpu-scaling-governor-on-ac (list "performance"))
+	      (sched-powersave-on-bat? #t)))
+;    (service zerotier-service-type
+;	     (zerotier-configuration
+;	      (network-id "3efa5cb78aa6d900")
+;	      (auto-join? #t)))
+    (service syncthing-service-type
+	     (syncthing-configuration (user "chend")))
+    (service screen-locker-service-type
+	     (screen-locker-configuration
+	      (name "swaylock")
+	      (program (file-append (specification->package "swaylock")
+				    "/bin/swaylock"))
+	      (allow-empty-password? #f)
+	      (using-pam? #t)
+	      (using-setuid? #f)))
+
+    (set-xorg-configuration
+     (xorg-configuration (keyboard-layout keyboard-layout)))
+
+    (modify-services %desktop-services
+		     (guix-service-type
+   		      config =>
+		      (guix-configuration
+		       (inherit config)
+		       (substitute-urls '("https://mirror.sjtu.edu.cn/guix/"
+					  "https://ci.guix.gnu.org"
+					  "https://bordeaux.guix.gnu.org"
+					  "https://substitutes.nonguix.org"))))
+		     (gdm-service-type
+		      config => (gdm-configuration
+				 (inherit config)
+				 (wayland? #t))))
+    ))
+
+  (bootloader (bootloader-configuration
+                (bootloader grub-efi-bootloader)
+                (targets (list "/boot/efi"))
+                (keyboard-layout keyboard-layout)))
+
+  ;; The list of file systems that get "mounted".  The unique
+  ;; file system identifiers there ("UUIDs") can be obtained
+  ;; by running 'blkid' in a terminal.
+  (file-systems (cons* (file-system
+                         (mount-point "/")
+                         (device (uuid
+                                  "2f8de685-5ade-40d6-9b22-d9729ae46e87"
+                                  'ext4))
+                         (type "ext4"))
+                       (file-system
+                         (mount-point "/boot/efi")
+                         (device (uuid "47CA-752C"
+                                       'fat32))
+                         (type "vfat")) %base-file-systems)))
---
+++ guix/G1a/home-configuration.scm
@@ -0,0 +1,43 @@
+;; This "home-environment" file can be passed to 'guix home reconfigure'
+;; to reproduce the content of your profile.  This is "symbolic": it only
+;; specifies package names.  To reproduce the exact same profile, you also
+;; need to capture the channels being used, as returned by "guix describe".
+;; See the "Replicating Guix" section in the manual.
+
+(use-modules (gnu home)
+             (gnu packages)
+             (gnu services)
+             (guix gexp)
+             (gnu home services shells))
+
+(home-environment
+  ;; Below is the list of packages that will show up in your
+  ;; Home profile, under ~/.guix-home/profile.
+  (packages (specifications->packages (list)))
+
+  ;; Below is the list of Home services.  To search for available
+  ;; services, run 'guix home search KEYWORD' in a terminal.
+  (services
+   (list (service home-bash-service-type
+                  (home-bash-configuration
+		   (guix-defaults? #t)
+		   (environment-variables
+		    '(
+		      ;; fcitx输入法
+		      ("GTK_IM_MODULE" . "fcitx")
+		      ("QT_IM_MODULE" . "fcitx")
+		      ("XMODIFILERS" . "@im=fcitx")
+		      ("SDL_IM_MODULE" . "fcitx")
+		      ("INPUT_METHOD" . "fcitx")
+		      ("GLFW_IM_MODULE" . "ibus")
+		      ("GUIX_GTK2_IM_MODULE_FILE" . "/run/current-system/profile/lib/gtk-2.0/2.10.0/immodules-gtk2.cache")
+		      ("GUIX_GTK3_IM_MODULE_FILE" . "/run/current-system/profile/lib/gtk-3.0/3.0.0/immodules-gtk3.cache")
+		      ))
+                   (aliases '(("grep" . "grep --color=auto") ("ll" . "ls -l")
+                              ("ls" . "ls -p --color=auto")))
+                   (bashrc (list (local-file
+                                  "/home/chend/src/guix-config/.bashrc"
+                                  "bashrc")))
+                   (bash-profile (list (local-file
+                                        "/home/chend/src/guix-config/.bash_profile"
+                                        "bash_profile"))))))))
